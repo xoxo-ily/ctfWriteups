f256074fd50e0b0edbd0c31e7fc19ab6d998b314
lisa <68173773+xoxo-ily@users.noreply.github.com>
2023-10-13 15:48:19 -0600

    Update atbash_decrypt.py

--- picoCTF/crypto/HideToSee/atbash_decrypt.py
+++ picoCTF/crypto/HideToSee/atbash_decrypt.py
@@ -1,20 +1,16 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 reverse = list(reversed(alphabet))
 
-def decrypt(msg):
-    new_msg = ''
-    msg = msg.split(' ')
-    for word in msg:
-        for letter in list(word):
-            if letter.lower() in alphabet:
-                index = alphabet.index(letter.lower())
-                new_letter = reverse[index]
-                if letter.isupper():
-                    new_letter = new_letter.upper()
-                new_msg += new_letter
-            else:
-                new_msg += letter
-        new_msg += ' '
-    print(new_msg)
+def atbash(msg):
+  new = ""
+  for l in list(msg):
+    if l.lower() in alphabet:
+      if l.isupper():
+        new += reverse[alphabet.index(l.lower())].upper()
+      else:
+        new += reverse[alphabet.index(l)]
+    else:
+      new += l
+  return new
 
-decrypt(input('enter encrypted message> '))
+print(atbash(input('enter encrypted message> ')))
